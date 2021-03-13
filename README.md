f460a5bed5b4ef9e57ad6f8f4ac8a405932e15d1
James Bell <jamesleonbell@gmail.com>
2021-03-13 11:34:23 -0800

    added more game code and edited the readme

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 This is the color game
 
 To play
-1.  TBD
-2.  TBD
-3.  TBD
+1.  Open your terminal window
+2.  Navigate to the colorgames directory
+3.  Enter "colorGames.py"
+
+
+Eventually we can make this game more complex by adding more features
+
+To Do:
+1. Add feature that announces how many tries it took to guess the number at end of game
+2. Add ability for user to select the guessing range
+
+
--- colorGame.py
+++ colorGame.py
@@ -1,2 +1,21 @@
 #This is my main code.
 
+import random
+
+randomNumber = random.randint(1,10)
+
+print ("Color games is now beginning! Try your wit and lets see how many times it takes you to guess...")
+
+guess = int(input("enter a number between 1 and 10: "))
+
+while randomNumber != guess:
+
+    if guess < randomNumber:
+        print ("Too low")
+        guess = int(input("Guess again: "))
+
+    elif guess > randomNumber:
+        print ("too high")
+        guess = int(input("Guess again?: "))
+
+print ("You guessed it. You win!")
